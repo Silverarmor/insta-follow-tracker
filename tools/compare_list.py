67f0ba72d709f241213fd5fa7ca3e54dcf854400
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2021-02-14 02:11:46 +1300

    Delete tools directory

--- tools/compare_list.py
+++
@@ -1,7 +0,0 @@
-list_a = ['Apple', 'Banana', 'Canteloupe']
-list_b = ['Apple', 'Banana', 'Delta']
-
-print(list((set(list_a) - set(list_b))))
-# --> ['Canteloupe']
-
-# print("These items are not in both lists: " missing_items_var)
