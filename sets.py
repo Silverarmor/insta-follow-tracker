53dd8cee11df5d7b0d07dd1024de37d7f700562c
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2021-02-20 00:53:37 +1300

    Delete sets.py

--- sets.py
+++
@@ -1,8 +0,0 @@
-
-old_followers = ["a", "b", "d", "e"]
-followers = ["b", "c", "d", "e"]
-
-nolonger_following_me = list(set(old_followers) - set(followers))
-print(nolonger_following_me)
-new_following_me = list(set(followers) - set(old_followers))
-print(new_following_me)
